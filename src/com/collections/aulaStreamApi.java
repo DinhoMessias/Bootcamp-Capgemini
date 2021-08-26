1c921798f400e81eaf47b273e52b18f8c6bb2283
DinhoMessias <dinhomessias@hotmail.com>
2021-08-26 20:44:57 -0300

    Commit com conteudo da primeira aula pratica de Stream API

---
+++ src/com/collections/aulaStreamApi.java
@@ -0,0 +1,108 @@
+package com.collections;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class aulaStreamApi {
+    /*Dadas as seguintes informações  de id e contato, crie um dicionário e
+ordene este dicionário exibindo (Nome id - Nome contato);
+id = 1 - Contato = nome: Simba, numero: 2222;
+id = 4 - Contato = nome: Cami, numero: 5555;
+id = 3 - Contato = nome: Jon, numero: 1111;
+*/
+    public static void main(String[] args) {
+        Map<Integer, Contato> agenda = new HashMap<>() {{
+            put(1, new Contato("Simba", 5555));
+            put(4, new Contato("Cami", 1111));
+            put(3, new Contato("Jon", 2222));
+        }};
+
+        System.out.println("--\tOrdem Id\t--");
+        //precisamos organizar os valores. Logo:
+        Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(new Comparator<Map.Entry<Integer, Contato>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Contato> c1, Map.Entry<Integer, Contato> c2) {
+                return Integer.compare(c1.getKey(), c2.getKey());
+            }
+        });
+        set.addAll(agenda.entrySet());
+        for (Map.Entry<Integer, Contato> entry : set) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNumero() +
+                    ": " + entry.getValue().getNome());
+        }
+
+        System.out.println("\n--\tOrdem Numero Telefone\t--");
+        //precisamos organizar os valores. Logo:
+        Set<Map.Entry<Integer, Contato>> set2 = new TreeSet<>(new Comparator<Map.Entry<Integer, Contato>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Contato> c1, Map.Entry<Integer, Contato> c2) {
+                return Integer.compare(c1.getValue().getNumero(), c2.getValue().getNumero());
+            }
+        });
+        set2.addAll(agenda.entrySet());
+        for (Map.Entry<Integer, Contato> entry : set2) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNumero() +
+                    ": " + entry.getValue().getNome());
+        }
+
+        System.out.println("\n--\tOrdem nome contato\t--");
+        //precisamos organizar os valores. Logo:
+        Set<Map.Entry<Integer, Contato>> set3 = new TreeSet<>(Comparator.comparing(new Function<Map.Entry<Integer, Contato>, String>() {
+            @Override
+            public String apply(Map.Entry<Integer, Contato> contato) {
+                return contato.getValue().getNome();
+            }
+        }));
+        set3.addAll(agenda.entrySet());
+        for (Map.Entry<Integer, Contato> entry : set3) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
+        }
+    }
+}
+
+class Contato {
+    private String nome;
+    private Integer numero;
+
+    public Contato(String nome, Integer numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return nome.equals(contato.nome) && numero.equals(contato.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numero);
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", numero='" + numero + '\'' +
+                '}';
+    }
+
+}
+
+class ComparatorOrdemNomeContato implements Comparator<Map.Entry<Integer, Contato>> {
+    @Override
+    public int compare(Map.Entry<Integer, Contato> cont1, Map.Entry<Integer, Contato> cont2) {
+        return cont1.getValue().getNome().compareToIgnoreCase(cont2.getValue().getNome());
+    }
+}
